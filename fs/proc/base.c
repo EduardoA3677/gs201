269f3f12c7165163a34e31926e17b959cd5c47aa
EduardoA3677 <ealvarado261500@gmail.com>
2025-04-23 19:35:35 -0600

    /proc/pid/maps: fake jit-zygote-cache flags

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2289,12 +2289,12 @@ static int map_files_get_link(struct dentry *dentry, struct path *path)
 	vma = find_exact_vma(mm, vm_start, vm_end);
 	if (vma) {
         if (vma->vm_file) {
-            if (strstr(vma->vm_file->f_path.dentry->d_name.name, "lineage")) { 
-				rc = kern_path("/system/framework/framework-res.apk", LOOKUP_FOLLOW, path);
-			} else {
-				*path = vma->vm_file->f_path;
-				path_get(path);
-				rc = 0;
+            if (strstr(vma->vm_file->f_path.dentry->d_name.name, "lineage")) {
+            	rc = kern_path("/dev/ashmem (deleted)", LOOKUP_FOLLOW, path);
+        	} else {
+			    *path = vma->vm_file->f_path;
+			    path_get(path);
+                rc = 0;
             }
         }
     }
